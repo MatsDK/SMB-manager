ee748f3dd976e32da14a72994a788022b4569d15
MatsDK <67562518+MatsDK@users.noreply.github.com>
2022-10-02 22:30:00 +0200

    store changed global params

--- src/components/GlobalConfigView.tsx
+++ src/components/GlobalConfigView.tsx
@@ -1,8 +1,8 @@
-import { useAtom } from 'jotai'
-import { globalParams } from '../../get-docs/parsedConfParams.json'
-import { GlobalConfigAtom } from '../utils/store'
-import { DashboardLayout } from './DashboardView'
+import { useAtom, useAtomValue, useSetAtom } from 'jotai'
 import PreactMarkdown from 'preact-markdown'
+import { globalParams } from '../../get-docs/parsedConfParams.json'
+import { changedGlobalFields, editGlobalConfigAtom, globalChangedAtom, GlobalConfigAtom } from '../utils/store'
+import { DashboardLayout } from './DashboardView'
 
 const Markdown = PreactMarkdown as any
 
@@ -10,11 +10,28 @@ const GlobalParamsKeys = Object.keys(globalParams)
 type GlobalDocsKeys = keyof typeof globalParams
 
 export const GlobalConfigView = () => {
-    const [globalConfig] = useAtom(GlobalConfigAtom)
+    const globalConfig = useAtomValue(GlobalConfigAtom)
+    const hasChanged = useAtomValue(globalChangedAtom)
+    const changedValues = useAtomValue(changedGlobalFields)
     if (!globalConfig) return null
+
+    const saveChanges = () => {
+        const newGlobalConfig = {
+            ...globalConfig,
+            ...changedValues
+        }
+
+
+    }
 
     return (
         <DashboardLayout>
+            {hasChanged && (
+                <div>
+                    Global changed
+                    <button onClick={saveChanges}>Save</button>
+                </div>
+            )}
             {GlobalParamsKeys.map((paramName) => {
                 if (!paramName) return
                 const value = globalConfig[paramName]
@@ -22,7 +39,7 @@ export const GlobalConfigView = () => {
                 return (
                     <ConfigParam
                         key={paramName}
-                        value={value}
+                        value={value || ''}
                         name={paramName as GlobalDocsKeys}
                     />
                 )
@@ -33,28 +50,33 @@ export const GlobalConfigView = () => {
 
 interface ConfigParamProps {
     name: GlobalDocsKeys
-    value: string | undefined
+    value: string
 }
 
 const ConfigParam = ({ name, value }: ConfigParamProps) => {
     const docs = globalParams[name]
+    const setGlobalConfig = useSetAtom(editGlobalConfigAtom)
+
     return (
-        <div className="my-4 py-2">
+        <div className='my-4 py-2'>
             <div className='flex mb-4'>
-                <span className="flex-1 font-medium text-primary-text text-lg">{name}</span>
-                <div className="flex-1">
+                <span className='flex-1 font-medium text-primary-text text-lg'>{name}</span>
+                <div className='flex-1'>
                     <input
                         placeholder={`Default: ${(docs as any).default}`}
-                        className="rounded-md bg-secondary-bg py-1 px-3 text-primary-text"
-                        type="text"
-                        value={value}
+                        className='rounded-md bg-secondary-bg py-1 px-3 text-primary-text'
+                        type='text'
+                        defaultValue={value}
                         title={(docs as any).default}
+                        onChange={(e) => setGlobalConfig(prev => {
+                            prev[name] = e.currentTarget.value
+                            return prev
+                        })}
                     />
                 </div>
             </div>
 
-
-            <div className="text-secondary-text">
+            <div className='text-secondary-text'>
                 <Markdown markdown={docs.md} />
             </div>
         </div>
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,10 +1,29 @@
 import { atom } from 'jotai'
 
+export type GlobalConfigType = Record<string, string>
 export type ConfigType =
     | {
-        'global': Record<string, string>
+        'global': GlobalConfigType
     } & Record<string, any>
     | null
 
 export const configAtom = atom({} as ConfigType)
 export const GlobalConfigAtom = atom((get) => get(configAtom)?.['global'])
+
+export const changedGlobalFields = atom({} as GlobalConfigType)
+export const globalChangedAtom = atom(false)
+
+export const editGlobalConfigAtom = atom(
+    null,
+    (get, set, update: (prev: GlobalConfigType) => GlobalConfigType) => {
+        const newVal = update(get(changedGlobalFields))
+        const hasChanged = compareFields(get(GlobalConfigAtom) || {}, newVal)
+        set(globalChangedAtom, hasChanged)
+        set(changedGlobalFields, newVal)
+    },
+)
+
+const compareFields = (initial: GlobalConfigType, newFields: GlobalConfigType) =>
+    Object.entries(newFields).some(
+        ([name, value]) => value !== (initial[name] || ''),
+    )
